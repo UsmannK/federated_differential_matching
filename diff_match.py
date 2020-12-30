212051078ac5073b3e912c814546c63b0d074e84
Usmann Khan <usmann@usmannkhan.com>
2020-12-29 23:16:00 -0800

    messing around with sinkhorn

--- diff_match.py
+++ diff_match.py
@@ -7,6 +7,10 @@ import torch.optim as optim
 import torch.nn as nn
 import numpy as np
 
+from geomloss import SamplesLoss
+from layers import SinkhornDistance
+import ot
+
 
 class MatchOneLayer(nn.Module):
     def __init__(self, input_size, output_size, num_models):
@@ -16,37 +20,53 @@ class MatchOneLayer(nn.Module):
         self.num_models = num_models
 
         self.pi_li = nn.Parameter(torch.rand(num_models, output_size, output_size))
+        self.global_model = nn.Parameter(torch.rand(self.input_size, self.output_size))
         self.log_softmax = nn.LogSoftmax(dim=1)
         self.pdist = nn.PairwiseDistance(p=2)
         self.softmax = nn.Softmax(dim=2)
-        
+        self.loss = SamplesLoss("sinkhorn", p=2, blur=.1)
+        self.sinkhorn = SinkhornDistance(eps=1, max_iter=100, reduction=None)
 
     def forward(self, layer_weights):
         # Cost: euclidean distance
-        pi_li_2 = self.softmax(self.pi_li)# ** 2
-        transported_layer_weights = torch.matmul(layer_weights, pi_li_2)
-        global_layer_weights = (torch.sum(transported_layer_weights,axis=0) / self.num_models).detach()
-        c = torch.cdist(global_layer_weights.T, layer_weights.permute(0,2,1)) #torch.cdist(global_layer_weights, layer_weights) # Get the euclidean distance from theta to local model weights
+        # pi_li_2 = self.softmax(self.pi_li)# ** 2
+        # pi_li_2 = self.pi_li
+        # for _ in range(100):
+        #     pi_li_2 = pi_li_2 - torch.logsumexp(pi_li_2, 2).reshape(-1, self.output_size, 1)
+        #     pi_li_2 = pi_li_2 - torch.logsumexp(pi_li_2, 1).reshape(-1, 1, self.output_size)
+        # pi_li_2 = torch.exp(pi_li_2)
+        # transported_layer_weights = torch.matmul(layer_weights, pi_li_2)
+        # global_layer_weights = (torch.sum(transported_layer_weights,axis=0) / self.num_models).detach()
+        # c = torch.cdist(global_layer_weights.T, layer_weights.permute(0,2,1)) #torch.cdist(global_layer_weights, layer_weights) # Get the euclidean distance from theta to local model weights
         # -log of cost
         # log_p = self.log_softmax(c)
         # neg_log_p = -1 * log_p
         # Sum across neurons and models
         # loss = torch.sum(pi_li_2/self.output_size * c) / self.num_models # Multiply our "permutation" matrix by -log(c) and square the sum, we want to minimize this
-        loss = self.pdist(transported_layer_weights[0].T, transported_layer_weights[1].T).sum()
+        # loss = self.pdist(transported_layer_weights[0].T, transported_layer_weights[1].T).sum()
+        loss, P, C = self.sinkhorn(layer_weights.permute(0,2,1), self.global_model.T.repeat(self.num_models, 1, 1))
+        # p0 = torch.zeros_like(P)
+
+        # transported_layer_weights = torch.matmul(layer_weights, P*self.output_size)
+        # global_layer_weights = (torch.sum(transported_layer_weights,axis=0) / self.num_models)
+        # loss = self.pdist(transported_layer_weights, global_layer_weights.repeat(self.num_models, 1, 1)).sum()
+        # for sub_matrix in transported_layer_weights:
+        #     loss += self.loss(global_layer_weights, sub_matrix)
+        
 
         # Col sums
-        col_sums = torch.sum(pi_li_2, dim=1)
-        col_diff_to_one = col_sums - torch.ones_like(col_sums)
-        squared_col_diff = torch.sum(col_diff_to_one ** 2)
+        # col_sums = torch.sum(pi_li_2, dim=1)
+        # col_diff_to_one = col_sums - torch.ones_like(col_sums)
+        # squared_col_diff = torch.sum(col_diff_to_one ** 2)
 
         # Row sums
         # row_sums = torch.sum(pi_li_2, dim=2)
         # row_diff_to_one = row_sums - torch.ones_like(row_sums)
         # squared_row_diff = torch.sum(row_diff_to_one ** 2)
 
-        post_loss = loss + squared_col_diff #+ squared_row_diff + squared_col_diff
+        post_loss = loss.sum() #+ squared_col_diff #+ squared_row_diff + squared_col_diff
 
-        return post_loss, [loss, 0, squared_col_diff], global_layer_weights.detach()
+        return post_loss, [loss, 0, 0], self.global_model.detach(), P
 
 def prepare_weights(models):
     weights = []
@@ -82,7 +102,7 @@ def get_matched_weights(cur_weights, layer_idx, args):
     optimizer = optim.Adam(match.parameters(), lr=args.lr)
     for epoch in range(args.diff_match_epochs):
         optimizer.zero_grad()
-        loss, losses_arr, global_layer_weights = match(layer_weights)
+        loss, losses_arr, global_layer_weights,pi_li = match(layer_weights)
         
         if best_loss is None or loss < best_loss:
             best_dict = copy.deepcopy(match.state_dict())
@@ -90,10 +110,14 @@ def get_matched_weights(cur_weights, layer_idx, args):
 
         loss.backward()
         optimizer.step()
+        
+        transported_layer_weights = torch.matmul(layer_weights, pi_li*match.output_size)
+        global_layer_weights = (torch.sum(transported_layer_weights,axis=0) / 2)
 
-        if (epoch+1) % 1000 == 0 or epoch == 0:
+        if (epoch+1) % 10 == 0 or epoch == 0:
             logging.debug(f'Epoch {epoch+1:5}')
             logging.debug(f'Loss: {loss}')
+            logging.debug(f'pdist: {[torch.nn.PairwiseDistance(p=2)(global_layer_weights.T, x.T).sum() for x in transported_layer_weights]}')
             logging.debug(f'Matching Loss: {losses_arr[0]}')
             logging.debug(f'Row Loss: {losses_arr[1]}')
             logging.debug(f'Col Loss: {losses_arr[2]}')
@@ -102,8 +126,29 @@ def get_matched_weights(cur_weights, layer_idx, args):
     match.load_state_dict(best_dict)
     if args.dump_intermediate_models:
         torch.save(match, Path(args.logdir)/f'match_{layer_idx}.pth')
-    loss, losses_arr, global_layer_weights = match(layer_weights)
-    pi_li = match.softmax(match.pi_li).detach().cpu()# ** 2
-    logging.debug(f'Best Loss (should be {best_loss})\nLoss: {loss}\nMatching Loss: {losses_arr[0]}\nRow Loss: {losses_arr[1]}\nCol Loss: {losses_arr[2]}\n')
+    loss, losses_arr, global_layer_weights, pi_li = match(layer_weights)
+
+    mu_cardinality, nu_cardinality = output_size,output_size
+    mu = torch.ones(mu_cardinality)/mu_cardinality
+    nu = torch.ones(nu_cardinality)/nu_cardinality
+
+    # _,pi_li ,_ = SinkhornDistance(eps=0.001, max_iter=1000, reduction=None)(layer_weights[0].T, layer_weights[1].T)
+    # import ot
+    # mu = torch.ones(output_size)/output_size
+    # nu = torch.ones(output_size)/output_size
+    # pi_li = torch.Tensor(ot.emd(mu,nu,torch.cdist(layer_weights[0].T, layer_weights[1].T)))
+    pi_li *= output_size
+    transported_layer_weights = torch.matmul(layer_weights, pi_li)#layer_weights.permute(0,2,1)).permute(0,2,1)
+    pi_li_ones = torch.zeros(2,100,100)
+    pi_li_ones[[0,1], torch.stack([torch.arange(0,100), torch.arange(0,100)]).T, pi_li.argmax(0).T] = 1
+    # transported_layer_weights = torch.matmul(layer_weights, pi_li)
+    global_layer_weights = (torch.sum(transported_layer_weights,axis=0) / len(transported_layer_weights))
+    # transport_matrix[pi_li]
+    # pi_li = match.pi_li.detach().cpu()# ** 2
+    # for _ in range(100):
+    #     pi_li = pi_li - torch.logsumexp(pi_li, 2).reshape(-1, match.output_size, 1)
+    #     pi_li = pi_li - torch.logsumexp(pi_li, 1).reshape(-1, 1, match.output_size)
+    # pi_li = torch.exp(pi_li)
+    # logging.debug(f'Best Loss (should be {best_loss})\nLoss: {loss}\nMatching Loss: {losses_arr[0]}\nRow Loss: {losses_arr[1]}\nCol Loss: {losses_arr[2]}\n')
 
     return global_layer_weights, pi_li
